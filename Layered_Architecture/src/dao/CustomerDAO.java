cb0b2d325542d0b3228f247c7d78b79149b6be18
sanuv9683 <sanuv9683@gmail.com>
2023-05-17 13:32:17 +0530

    Content Updated with first method GetAllCustomers

---
+++ Layered_Architecture/src/dao/CustomerDAO.java
@@ -0,0 +1,29 @@
+package dao;
+
+import db.DBConnection;
+import model.CustomerDTO;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class CustomerDAO {
+
+    public ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        Statement stm = connection.createStatement();
+        ArrayList<CustomerDTO> allCustomers = new ArrayList<>();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
+        while (rst.next()) {
+            String id = rst.getString(0);
+            String name = rst.getString(1);
+            String address = rst.getString(2);
+            CustomerDTO customerDTO = new CustomerDTO(id, name, address);
+            allCustomers.add(customerDTO);
+        }
+        return allCustomers;
+    }
+}
