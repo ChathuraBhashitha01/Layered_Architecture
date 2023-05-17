b4719393a2e0333f5d7a45b7cfa2cb526a3b62d5
sanuv9683 <sanuv9683@gmail.com>
2023-05-17 14:53:53 +0530

    ItemDAO delete method refactored in both DAO and Controller

--- Layered_Architecture/src/dao/ItemDAO.java
+++ Layered_Architecture/src/dao/ItemDAO.java
@@ -4,10 +4,7 @@ import db.DBConnection;
 import model.ItemDTO;
 
 import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class ItemDAO {
@@ -25,5 +22,12 @@ public class ItemDAO {
             allItems.add(itemDTO);
         }
         return allItems;
+    }
+
+    public boolean deleteItem(String code) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("DELETE FROM Item WHERE code=?");
+        pstm.setString(1, code);
+        return pstm.executeUpdate()>0;
     }
 }
