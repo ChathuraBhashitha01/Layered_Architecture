7bbcbdd2e6bb67e481c30ff0fbacb4b4c91e9ee0
sanuv9683 <sanuv9683@gmail.com>
2023-05-17 14:22:01 +0530

    Customer DAO delete method refactored with CustomerController

--- Layered_Architecture/src/controller/ManageCustomersFormController.java
+++ Layered_Architecture/src/controller/ManageCustomersFormController.java
@@ -197,10 +197,9 @@ public class ManageCustomersFormController {
             if (!existCustomer(id)) {
                 new Alert(Alert.AlertType.ERROR, "There is no such customer associated with the id " + id).show();
             }
-            Connection connection = DBConnection.getDbConnection().getConnection();
-            PreparedStatement pstm = connection.prepareStatement("DELETE FROM Customer WHERE id=?");
-            pstm.setString(1, id);
-            pstm.executeUpdate();
+
+            CustomerDAO customerDAO = new CustomerDAO();
+            customerDAO.deleteCustomer(id);
 
             tblCustomers.getItems().remove(tblCustomers.getSelectionModel().getSelectedItem());
             tblCustomers.getSelectionModel().clearSelection();
