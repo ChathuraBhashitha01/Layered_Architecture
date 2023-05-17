54aa01702b675b2f5bcbe74ee40fc03dc0cb3d7d
sanuv9683 <sanuv9683@gmail.com>
2023-05-17 14:56:00 +0530

    Content Updated with ItemSave on ItemDAO and ItemController

--- Layered_Architecture/src/controller/ManageItemsFormController.java
+++ Layered_Architecture/src/controller/ManageItemsFormController.java
@@ -176,13 +176,9 @@ public class ManageItemsFormController {
                     new Alert(Alert.AlertType.ERROR, code + " already exists").show();
                 }
                 //Save Item
-                Connection connection = DBConnection.getDbConnection().getConnection();
-                PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)");
-                pstm.setString(1, code);
-                pstm.setString(2, description);
-                pstm.setBigDecimal(3, unitPrice);
-                pstm.setInt(4, qtyOnHand);
-                pstm.executeUpdate();
+                ItemDAO itemDAO = new ItemDAO();
+                itemDAO.saveItem(new ItemDTO(code,description,unitPrice,qtyOnHand));
+
                 tblItems.getItems().add(new ItemTM(code, description, unitPrice, qtyOnHand));
 
             } catch (SQLException e) {
--- Layered_Architecture/src/dao/ItemDAO.java
+++ Layered_Architecture/src/dao/ItemDAO.java
@@ -30,4 +30,14 @@ public class ItemDAO {
         pstm.setString(1, code);
         return pstm.executeUpdate()>0;
     }
+
+    public boolean saveItem(ItemDTO dto) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)");
+        pstm.setString(1, dto.getCode());
+        pstm.setString(2, dto.getDescription());
+        pstm.setBigDecimal(3, dto.getUnitPrice());
+        pstm.setInt(4, dto.getQtyOnHand());
+        return pstm.executeUpdate()>0;
+    }
 }
