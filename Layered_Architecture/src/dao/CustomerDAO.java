a1dcfa3443a8a9e536578765d3148f0d5e01c955
sanuv9683 <sanuv9683@gmail.com>
2023-05-17 13:43:37 +0530

    Bug Fixed

--- Layered_Architecture/src/dao/CustomerDAO.java
+++ Layered_Architecture/src/dao/CustomerDAO.java
@@ -18,9 +18,9 @@ public class CustomerDAO {
         ArrayList<CustomerDTO> allCustomers = new ArrayList<>();
         ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
         while (rst.next()) {
-            String id = rst.getString(0);
-            String name = rst.getString(1);
-            String address = rst.getString(2);
+            String id = rst.getString(1);
+            String name = rst.getString(2);
+            String address = rst.getString(3);
             CustomerDTO customerDTO = new CustomerDTO(id, name, address);
             allCustomers.add(customerDTO);
         }
