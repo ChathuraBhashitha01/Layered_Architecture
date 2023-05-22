91124a9a478045146e58566123680a80e7f68e83
sanuv9683 <sanuv9683@gmail.com>
2023-05-22 14:04:17 +0530

    Added ItemDAO interface to loosecouple ItemDAOImpl

--- Layered_Architecture/src/dao/ItemDAOImpl.java
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -5,7 +5,8 @@ import model.ItemDTO;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class ItemDAOImpl {
+public class ItemDAOImpl implements ItemDAO{
+    @Override
     public ArrayList<ItemDTO> getAllItems() throws SQLException, ClassNotFoundException {
         ResultSet rst = SQLUtil.execute("SELECT * FROM Item");
         ArrayList<ItemDTO> allItems = new ArrayList<>();
@@ -15,23 +16,28 @@ public class ItemDAOImpl {
         return allItems;
     }
 
+    @Override
     public boolean deleteItem(String code) throws SQLException, ClassNotFoundException {
         return SQLUtil.execute("DELETE FROM Item WHERE code=?",code);
     }
 
+    @Override
     public boolean saveItem(ItemDTO dto) throws SQLException, ClassNotFoundException {
        return SQLUtil.execute("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)",dto.getCode(),dto.getDescription(),dto.getUnitPrice(),dto.getQtyOnHand());
     }
 
+    @Override
     public boolean updateItem(ItemDTO dto) throws SQLException, ClassNotFoundException {
         return SQLUtil.execute("UPDATE Item SET description=?, unitPrice=?, qtyOnHand=? WHERE code=?",dto.getDescription(), dto.getUnitPrice(),dto.getQtyOnHand(), dto.getCode());
     }
 
+    @Override
     public boolean existItem(String code) throws SQLException, ClassNotFoundException {
         ResultSet rst = SQLUtil.execute("SELECT code FROM Item WHERE code=?",code);
         return rst.next();
     }
 
+    @Override
     public String generateNewCode() throws SQLException, ClassNotFoundException {
         ResultSet rst = SQLUtil.execute("SELECT code FROM Item ORDER BY code DESC LIMIT 1;");
         if (rst.next()) {
@@ -43,6 +49,7 @@ public class ItemDAOImpl {
         }
     }
 
+    @Override
     public ItemDTO searchItem(String code) throws SQLException, ClassNotFoundException {
         ResultSet rst= SQLUtil.execute("SELECT * FROM Item WHERE code=?",code);
         if (rst.next()){
