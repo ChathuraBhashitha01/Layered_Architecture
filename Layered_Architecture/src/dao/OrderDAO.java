85bd2a07935cadad1d354cdae547be522826c476
sanuv9683 <sanuv9683@gmail.com>
2023-05-22 12:02:01 +0530

    Refactored OrderDAO and OrderDetailsDAO with SQLUtil to reduce the boilerplate codes

--- Layered_Architecture/src/dao/OrderDAO.java
+++ Layered_Architecture/src/dao/OrderDAO.java
@@ -1,32 +1,24 @@
 package dao;
 
-import db.DBConnection;
 import model.OrderDTO;
 
-import java.sql.*;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class OrderDAO {
     public String generateNewOID() throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        Statement stm = connection.createStatement();
-        ResultSet rst = stm.executeQuery("SELECT oid FROM `Orders` ORDER BY oid DESC LIMIT 1;");
+        ResultSet rst = SQLUtil.execute("SELECT oid FROM `Orders` ORDER BY oid DESC LIMIT 1;");
         return rst.next() ? String.format("OID-%03d", (Integer.parseInt(rst.getString("oid").replace("OID-", "")) + 1)) : "OID-001";
     }
 
     public boolean existOrder(String orderID) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement stm = connection.prepareStatement("SELECT oid FROM `Orders` WHERE oid=?");
-        stm.setString(1, orderID);
-        return stm.executeQuery().next();
+        ResultSet rst = SQLUtil.execute("SELECT oid FROM `Orders` WHERE oid=?",orderID);
+        return rst.next();
     }
 
     public boolean saveOrder(OrderDTO dto) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement stm = connection.prepareStatement("INSERT INTO `Orders` (oid, date, customerID) VALUES (?,?,?)");
-        stm.setString(1, dto.getOrderId());
-        stm.setDate(2, Date.valueOf(dto.getOrderDate()));
-        stm.setString(3, dto.getCustomerId());
-        return stm.executeUpdate() > 0;
+        return SQLUtil.execute("INSERT INTO `Orders` (oid, date, customerID) VALUES (?,?,?)",dto.getOrderId(),Date.valueOf(dto.getOrderDate()),dto.getCustomerId());
     }
 
 }
--- Layered_Architecture/src/dao/OrderDetailsDAO.java
+++ Layered_Architecture/src/dao/OrderDetailsDAO.java
@@ -9,12 +9,6 @@ import java.sql.SQLException;
 
 public class OrderDetailsDAO {
  public boolean saveOrderDetails(OrderDetailDTO dto) throws SQLException, ClassNotFoundException {
-     Connection connection = DBConnection.getDbConnection().getConnection();
-     PreparedStatement stm = connection.prepareStatement("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)");
-     stm.setString(1, dto.getOrderID());
-     stm.setString(2, dto.getItemCode());
-     stm.setBigDecimal(3, dto.getUnitPrice());
-     stm.setInt(4, dto.getQty());
-     return stm.executeUpdate()>0;
+     return SQLUtil.execute("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)",dto.getOrderID(),dto.getItemCode(),dto.getUnitPrice(),dto.getQty());
  }
 }
