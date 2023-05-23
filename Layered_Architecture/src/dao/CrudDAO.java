124f2098dda756b0d9d30d1a23e4b41c1d690762
sanuv9683 <sanuv9683@gmail.com>
2023-05-23 09:15:37 +0530

    Removed public keyword from the interface methods

--- Layered_Architecture/src/dao/CrudDAO.java
+++ Layered_Architecture/src/dao/CrudDAO.java
@@ -1,16 +1,20 @@
 package dao;
 
-import model.CustomerDTO;
-
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public interface CrudDAO<T,ID> {
-    public ArrayList<T> getAll() throws SQLException, ClassNotFoundException;
-    public boolean save(T dto) throws SQLException, ClassNotFoundException;
-    public boolean update(T dto) throws SQLException, ClassNotFoundException;
-    public boolean exist(ID id) throws SQLException, ClassNotFoundException;
-    public boolean delete(ID id) throws SQLException, ClassNotFoundException;
-    public String generateNewID() throws SQLException, ClassNotFoundException;
-    public T search(ID id) throws SQLException, ClassNotFoundException;
+public interface CrudDAO<T, ID> {
+    ArrayList<T> getAll() throws SQLException, ClassNotFoundException;
+
+    boolean save(T dto) throws SQLException, ClassNotFoundException;
+
+    boolean update(T dto) throws SQLException, ClassNotFoundException;
+
+    boolean exist(ID id) throws SQLException, ClassNotFoundException;
+
+    boolean delete(ID id) throws SQLException, ClassNotFoundException;
+
+    String generateNewID() throws SQLException, ClassNotFoundException;
+
+    T search(ID id) throws SQLException, ClassNotFoundException;
 }
