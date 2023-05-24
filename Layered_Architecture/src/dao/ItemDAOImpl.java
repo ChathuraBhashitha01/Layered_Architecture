7e38e487f04da12e76510636f039f0ee4636b2d0
sanuv9683 <sanuv9683@gmail.com>
2023-05-24 10:00:36 +0530

    Content Updated with ItemDAO interdface for Item Table Unique methods

--- Layered_Architecture/src/dao/ItemDAOImpl.java
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class ItemDAOImpl implements CrudDAO<ItemDTO, String> {
+public class ItemDAOImpl implements ItemDAO {
     @Override
     public ArrayList<ItemDTO> getAll() throws SQLException, ClassNotFoundException {
         ResultSet rst = SQLUtil.execute("SELECT * FROM Item");
